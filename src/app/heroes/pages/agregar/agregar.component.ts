abf2c4eaab126980324d2eaf533ae9b85103ba27
hector.romang <hector.romang@durango.gob.mx>
2022-08-29 16:12:44 -0500

    Final de proyecti

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
 import { switchMap } from 'rxjs/operators';
@@ -7,7 +7,12 @@ import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
+  styles: [`
+    img{
+      width: 50%;
+      border-radius: 5px;
+    }
+    `
   ]
 })
 export class AgregarComponent implements OnInit {
@@ -33,9 +38,14 @@ export class AgregarComponent implements OnInit {
   }
 
   constructor( private heroeService: HeroesService,
-               private activatedRoute: ActivatedRoute ) { }
+               private activatedRoute: ActivatedRoute,
+               private router: Router ) { }
 
   ngOnInit(): void {
+
+    if(!this.router.url.includes('editar')){
+      return;
+    }
     this.activatedRoute.params
                        .pipe(switchMap(({ id }) => this.heroeService.getHeroePorId(id)))
                        .subscribe(heroe => this.heroe = heroe);
@@ -46,12 +56,21 @@ export class AgregarComponent implements OnInit {
       return;
     }
 
+    if(this.heroe.id){
+      this.heroeService.editarHeroe(this.heroe).subscribe()
+    }else{
+      this.heroeService.agregarHeroe(this.heroe).subscribe(rersp =>{
+        console.log(rersp);
+      }, err => {
+        console.log(err);
+      });
+    }
 
-    this.heroeService.agregarHeroe(this.heroe).subscribe(rersp =>{
-      console.log(rersp);
-    }, err => {
-      console.log(err);
-    })
   }
 
+  eliminar(heroe: Heroe): void{
+    this.heroeService.eliminarHeroe(heroe.id!).subscribe(success => {
+      this.router.navigate(['/heroes/listado'])
+    })
+  }
 }
